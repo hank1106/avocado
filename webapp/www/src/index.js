7cd5d5545c0852faf43ee96bde84a132ee02690d
hank1106 <hankzhang@zihans-mbp-2.lan>
2019-04-05 00:54:57 -0700

    Added basscss

--- webapp/www/src/index.js
+++ webapp/www/src/index.js
@@ -7,6 +7,8 @@ import * as serviceWorker from './serviceWorker';
 import 'jquery/dist/jquery.js';
 import 'popper.js/dist/umd/popper.js';
 import 'bootstrap/dist/js/bootstrap.js';
+import 'basscss/css/basscss.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
