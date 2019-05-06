5181ef242cce7fc20ebeebde408952e93b6fb596
hank1106 <hankcheung1106@gmail.com>
2019-05-05 22:42:04 -0700

    Added bookmark and rating buttons

--- webapp/www/src/components/Album.js
+++ webapp/www/src/components/Album.js
@@ -3,7 +3,10 @@ import logo from '../logo.svg';
 import AlbumProfileWidget from './AlbumProfileWidget.js';
 import AlbumIntro from './AlbumIntro.js';
 import AlbumRatingWidget from './AlbumRatingWidget.js';
-import AlbumTrackList from './AlbumTrackList.js'
+import AlbumTrackList from './AlbumTrackList.js';
+import Rater from 'react-rater';
+import 'react-rater/lib/react-rater.css';
+import '../css/Album.css';
 
 class Album extends Component {
   render() {
@@ -13,6 +16,13 @@ class Album extends Component {
           <AlbumProfileWidget id={this.props.match.params.id} />
           <div className="mt3">
             <AlbumRatingWidget />
+          </div>
+        </div>
+        <div className="flex justify-start mt3">
+          <button className="btn bookmark_button">Bookmark</button>
+          <div className="ml2 mr1">Rating: </div>
+          <div className="rater_div">
+            <Rater total={5} rating={0}/>
           </div>
         </div>
         <AlbumIntro id={this.props.match.params.id} />
